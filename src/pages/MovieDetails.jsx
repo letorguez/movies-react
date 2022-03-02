34e2bfb39a0a2883b791bb6a55bb0b67b5582b97
Leto <leto4100@gmail.com>
2022-03-02 17:43:03 +0000

    desarrollo 2

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,30 @@
-import pelicula from "./movie.json";
+import { get } from "../utils/httpClient";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 import styles from "./MovieDetails.module.css";
 
 export function MovieDetails() {
-  const imageUrl = "https://image.tmdb.org/t/p/w500" + pelicula.poster_path;
   const format = new Intl.ListFormat("en");
+  const { movieId } = useParams();
+  const [isLoading, setIsLoading] = useState(true)
+  const [pelicula, setMovie] = useState(null);
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+    });
+  }, [movieId]);
+
+  if (!pelicula) {
+    return null;
+  }
+  const imageUrl = "https://image.tmdb.org/t/p/w500" + pelicula.poster_path;
   return (
     <div className={styles.detailsContainer}>
-      <img src={imageUrl} alt={pelicula.title} className={styles.col + " " + styles.movieImage} />
+      <img
+        src={imageUrl}
+        alt={pelicula.title}
+        className={styles.col + " " + styles.movieImage}
+      />
       <div className={styles.col + " " + styles.movieDetails}>
         <p className={styles.firstItem}>
           <strong>Title: </strong>
